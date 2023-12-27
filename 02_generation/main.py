6dea9a1343c14c25c89198890b4a65119b3cadd3
Yehor Chernetskii <egorchernetskii@gmail.com>
2023-12-27 14:11:28 +0200

    generation_list_added

--- 02_generation/main.py
+++ 02_generation/main.py
@@ -1,1 +1,15 @@
 # TODO здесь писать код
+
+def generate_list(n):
+    result_list = []
+    for i in range(n):
+        if i % 2 == 0:
+            result_list.append(1)
+        else:
+            result_list.append(i % 5)
+    return result_list
+
+length_of_list = int(input("Введите длину списка: "))
+
+result = generate_list(length_of_list)
+print(f"Результат: {result}")
