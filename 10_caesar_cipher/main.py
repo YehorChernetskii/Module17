99bb7423256a399b135c239a44555fb43bed8eee
Yehor Chernetskii <egorchernetskii@gmail.com>
2024-01-02 16:02:29 +0200

    encrypt_caesar_added

--- 10_caesar_cipher/main.py
+++ 10_caesar_cipher/main.py
@@ -1,1 +1,21 @@
 # TODO здесь писать код
+
+def encrypt_caesar_russian(message, shift):
+    russian_alphabet = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+    encrypted_message = ''
+
+    for char in message:
+        if char.isalpha() and char.lower() in russian_alphabet:
+            is_upper = char.isupper()
+            char_index = (russian_alphabet.index(char.lower()) + shift) % 32
+            encrypted_char = russian_alphabet[char_index]
+            encrypted_message += encrypted_char.upper() if is_upper else encrypted_char
+        else:
+            encrypted_message += char
+
+    return encrypted_message
+
+message = input("Введите сообщение: ")
+shift = int(input("Введите сдвиг: "))
+result = encrypt_caesar_russian(message, shift)
+print("Зашифрованное сообщение:", result)
