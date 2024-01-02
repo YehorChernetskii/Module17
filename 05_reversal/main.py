b4922ec1c52a6ffb9a471ac2d78cf133c5aaccec
Yehor Chernetskii <egorchernetskii@gmail.com>
2024-01-02 15:01:59 +0200

    reverse_between_h_added

--- 05_reversal/main.py
+++ 05_reversal/main.py
@@ -1,1 +1,19 @@
 # TODO здесь писать код
+
+def reverse_between_h(input_string):
+    first_h_index = input_string.find('h')
+    last_h_index = input_string.rfind('h')
+
+    if first_h_index != -1 and last_h_index != -1 and first_h_index < last_h_index:
+        prefix = input_string[:first_h_index + 1]
+        suffix = input_string[last_h_index:]
+        middle_part = input_string[first_h_index + 1:last_h_index][::-1]
+
+        result = prefix + middle_part + suffix
+        return result
+    else:
+        return "Ошибка: не найдены две буквы 'h' или первая буква 'h' расположена после последней."
+
+user_input = input("Введите строку: ")
+result = reverse_between_h(user_input)
+print(result)
