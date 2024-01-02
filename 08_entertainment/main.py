9de102b102e1b34c77582e321678ec5465fac886
Yehor Chernetskii <egorchernetskii@gmail.com>
2024-01-02 15:41:15 +0200

    stick_game_added

--- 08_entertainment/main.py
+++ 08_entertainment/main.py
@@ -1,1 +1,14 @@
 # TODO здесь писать код
+
+N = int(input("Кол-во палок: "))
+K = int(input("Кол-во бросков: "))
+
+sticks = ['I'] * N
+
+for _ in range(K):
+    left, right = map(int, input("Бросок. Сбиты палки с номера до номера: ").split())
+    for i in range(left - 1, right):
+        sticks[i] = '.'
+
+result = ''.join(sticks)
+print("Результат:", result)
