2a0f99f7d7c0fadf30f76ead962c0407e3727288
Yehor Chernetskii <egorchernetskii@gmail.com>
2024-01-02 15:47:49 +0200

    nice_list_added

--- 09_list_of_lists_2/main.py
+++ 09_list_of_lists_2/main.py
@@ -2,3 +2,9 @@ nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
 # TODO здесь писать код
+
+nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
+
+flattened_list = [element for sublist1 in nice_list for sublist2 in sublist1 for element in sublist2]
+
+print(flattened_list)
