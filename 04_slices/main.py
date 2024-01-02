f2cb12b3e4a5e540fcd5f0e261a6846b4de52c7f
Yehor Chernetskii <egorchernetskii@gmail.com>
2024-01-02 14:52:24 +0200

    slices_added

--- 04_slices/main.py
+++ 04_slices/main.py
@@ -1,3 +1,35 @@
 alphabet = 'abcdefg'
 
 # TODO здесь писать код
+
+alphabet = 'abcdefg'
+
+# 1
+print(f'1: {alphabet}')
+
+# 2
+print(f'2: {alphabet[::-1]}')
+
+# 3
+print(f'3: {alphabet[::2]}')
+
+# 4
+print(f'4: {alphabet[1::2]}')
+
+# 5
+print(f'5: {alphabet[:2]}')
+
+# 6
+print(f'6: {alphabet[-1:-3:-1]}')
+
+# 7
+print(f'7: {alphabet[3:4]}')
+
+# 8
+print(f'8: {alphabet[-3:]}')
+
+# 9
+print(f'9: {alphabet[3:5]}')
+
+# 10
+print(f'10: {alphabet[3:5][::-1]}')
