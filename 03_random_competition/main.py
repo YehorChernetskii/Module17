5f15a279552eff12ce6affea84561b67ec227f1c
Yehor Chernetskii <egorchernetskii@gmail.com>
2024-01-02 14:41:43 +0200

    teams_competitions_added

--- 03_random_competition/main.py
+++ 03_random_competition/main.py
@@ -1,1 +1,13 @@
 # TODO здесь писать код
+
+import random
+
+first_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
+second_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
+
+print("Первая команда:", first_team)
+print("Вторая команда:", second_team)
+
+winners = [max(a, b) for a, b in zip(first_team, second_team)]
+
+print("Победители тура:", winners)
